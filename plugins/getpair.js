3df8d38b66164bed9ac7a30048df91b7fd9daf70
dagaya212 <dagayaking212@gmail.com>
2025-03-18 14:56:04 +0530

    Update getpair.js

--- plugins/getpair.js
+++ plugins/getpair.js
@@ -10,7 +10,7 @@ cmd({
     react: "✅",
     desc: "Pairing code",
     category: "download",
-    use: ".pair ++923477868XXX",
+    use: ".pair +9470XXXXXXX",
     filename: __filename
 }, 
 async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Success response
         const pairingCode = pair.code;
-        const doneMessage = "> *KHAN-MD PAIR COMPLETED*";
+        const doneMessage = "> *DAHAYA-MD PAIR COMPLETED*";
 
         // Send first message
         await reply(`${doneMessage}\n\n*Your pairing code is:* ${pairingCode}`);
